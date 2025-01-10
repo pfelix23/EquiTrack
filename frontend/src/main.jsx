ffd72b60c31721327a0dc15693e267aff07086c8
pfelix23 <peter.felix23@gmail.com>
2025-01-09 22:11:11 -0500

    finished backend added user/edit+/delete. working on fronted

---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { Provider } from 'react-redux';
+import { restoreCSRF, csrfFetch } from './store/csrf';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
