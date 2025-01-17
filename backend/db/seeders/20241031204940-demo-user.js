53e97a897a51fe7d5278875f6007a093488437e0
pfelix23 <peter.felix23@gmail.com>
2025-01-17 09:33:13 -0500

    Folders back and added tablename

--- backend/db/seeders/20241031204940-demo-user.js
+++ backend/db/seeders/20241031204940-demo-user.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'Users'
-    await queryInterface.bulkCreate(options, [
+    await queryInterface.bulkInsert(options.tableName, [
       {
         username: 'johnDoe',
         firstName: 'John',
